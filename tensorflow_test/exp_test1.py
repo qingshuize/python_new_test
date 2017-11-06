d3e144760065ba59ea8e3ca5a31f6d4978c4d9da
刘樊 <lf@qimingpian.com>
2017-11-06 11:15:49 +0800

    update

--- tensorflow_test/exp_test1.py
+++ tensorflow_test/exp_test1.py
@@ -13,6 +13,13 @@ def func():
         for _ in range(5):
             result=se.run(update)
             print(result)
-
+def func1():
+    x=ten.placeholder(ten.float16)
+    y=ten.placeholder(ten.float16)
+    output=ten.add(x,y)
+    with ten.Session() as se:
+        a=se.run([output], feed_dict={x:[num.random.random(1)],y:[num.random.random(1)]})
+        print(a)
 if __name__ == '__main__':
-    func()
+    #func()
+    func1()
