47fd5255ed3b703c920aa440d192a6b0fe2879cc
liufan <lf@qimingpian.com>
2018-04-12 13:36:41 +0800

    add tensorflow test

---
+++ tensorflow_test/tf_test/nearest_neighbor.py
@@ -0,0 +1,46 @@
+#coding:utf8
+import tensorflow as tf
+import numpy as np
+
+# Import MINST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("data/mnist", one_hot=True)
+# Extracting MNIST_data/train-images-idx3-ubyte.gz
+# Extracting MNIST_data/train-labels-idx1-ubyte.gz
+# Extracting MNIST_data/t10k-images-idx3-ubyte.gz
+# Extracting MNIST_data/t10k-labels-idx1-ubyte.gz
+
+# In this example, we limit mnist data
+Xtr, Ytr = mnist.train.next_batch(5000) #5000 for training (nn candidates)
+Xte, Yte = mnist.test.next_batch(200) #200 for testing
+
+# tf Graph Input
+xtr = tf.placeholder("float", [None, 784])
+xte = tf.placeholder("float", [784])
+
+# Nearest Neighbor calculation using L1 Distance
+# Calculate L1 Distance
+distance = tf.reduce_sum(tf.abs(tf.add(xtr, tf.negative(xte))), reduction_indices=1)
+# Prediction: Get min distance index (Nearest neighbor)
+pred = tf.argmin(distance, 0)
+
+accuracy = 0.
+
+# Initialize the variables (i.e. assign their default value)
+init = tf.global_variables_initializer()
+# Start training
+with tf.Session() as sess:
+    sess.run(init)
+
+    # loop over test data
+    for i in range(len(Xte)):
+        # Get nearest neighbor
+        nn_index = sess.run(pred, feed_dict={xtr: Xtr, xte: Xte[i, :]})
+        # Get nearest neighbor class label and compare it to its true label
+        print "Test", i, "Prediction:", np.argmax(Ytr[nn_index]), \
+            "True Class:", np.argmax(Yte[i])
+        # Calculate accuracy
+        if np.argmax(Ytr[nn_index]) == np.argmax(Yte[i]):
+            accuracy += 1./len(Xte)
+    print "Done!"
+    print "Accuracy:", accuracy
